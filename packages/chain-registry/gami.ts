1cd5416959aae099aae87c424c2fa2d411c6af49
Marcus Mattus <57033847+marcusmattus@users.noreply.github.com>
2026-04-28 08:33:19 +0100

    Add Gami Chain registry config

---
+++ packages/chain-registry/gami.ts
@@ -0,0 +1,15 @@
+export const GAMI_TESTNET = {
+  chainId: 74331,
+  name: 'Gami Chain Testnet',
+  symbol: 'tGAMI',
+  rpc: 'https://rpc-testnet.gamiprotocol.xyz',
+  explorer: 'https://explorer-testnet.gamiprotocol.xyz'
+};
+
+export const GAMI_MAINNET = {
+  chainId: 7433,
+  name: 'Gami Chain',
+  symbol: 'GAMI',
+  rpc: 'https://rpc.gamiprotocol.xyz',
+  explorer: 'https://explorer.gamiprotocol.xyz'
+};
